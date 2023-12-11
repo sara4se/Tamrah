8abb60da2d3caea6a0b6279a4af6411b9edea1c5
sara <sara.ahumidi4@gmail.com>
2023-12-12 00:51:02 +0300

    Initial Commit

---
+++ Tamrah/TamrahApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamrahApp.swift
+//  Tamrah
+//
+//  Created by Sara Alhumidi on 28/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TamrahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
